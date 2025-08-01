d77bcf572e933bbf404ea079fe8708d165464a93
Aditya Pratap Singh <Adityacu2026@gmail.com>
2025-08-01 18:34:35 +0530

    Update axiosInstance.js

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"https://url-shortner-backend-vbb6.onrender.com",
+    baseURL:"https://url-shortner-backend1-fv5j.onrender.com",
     timeout:10000, //10s
     withCredentials:true
 })
