25a7d84fd506e6a5a04adb4b1383b5d2eb5046a2
Aditya Pratap Singh <adityacu2026@gmail.com>
2025-08-01 18:30:37 +0530

    Feat: Configure frontend Axios instance

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -17,7 +17,7 @@ axiosInstance.interceptors.response.use(
         if (error.response) {
             // The server responded with a status code outside the 2xx range
             const { status, data } = error.response;
-            
+
             switch (status) {
                 case 400:
                     console.error("Bad Request:", data);
